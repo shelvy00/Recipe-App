1ebdbfe341ee43a14015e9ee1f149af9fc8b9dc9
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2022-02-24 21:32:53 -0600

    update.

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-let title = document.getElementById("title");
-//let img = document.getElementById("img").src = 
-let instructions = document.getElementById("instructions");
-let listElement = document.querySelector("ul");
-
-
-/*let	p1 = document.getElementById("p1");
-let	p2 = document.getElementById("p2");
-let	p3 = document.getElementById("p3");
-let	p4 = document.getElementById("p4");
-let	p5 = document.getElementById("p5");
-let	p6 = document.getElementById("p6");
-let	p7 = document.getElementById("p7");
-let	p8 = document.getElementById("p8");
-let	p9 = document.getElementById("p9");
-let	p10 = document.getElementById("p10");
-let	p11 = document.getElementById("p11");
-let	p12 = document.getElementById("p12");
-let	p13 = document.getElementById("p13");
-let	p14 = document.getElementById("p14");
-let	p15 = document.getElementById("p15");
-let	p16 = document.getElementById("p16");
-let	p17 = document.getElementById("p17");
-let	p18 = document.getElementById("p18");
-let	p19 = document.getElementById("p19");
-let	p20 = document.getElementById("p20");*/
-let video = document.getElementById("video");
+const title = document.getElementById("title");
+const instructions = document.getElementById("instructions");
+const listElement = document.querySelector("ul");
+const video = document.getElementById("video");
 
  axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
         .then(function (response) {
@@ -45,7 +22,7 @@ let video = document.getElementById("video");
 
 //Image of meal
 function recipeImage(image) {
-  let img = document.getElementById("img").src = image.strMealThumb
+  const img = document.getElementById("img").src = image.strMealThumb
 
 };
 
@@ -55,7 +32,7 @@ function recipeInstructions(Ins) {
 };
 
 // ingredients
-function recipeIngredients(ingred) {
+/*function recipeIngredients(ingred) {
     p1.innerHTML = ingred.strIngredient1
     p2.innerHTML = ingred.strIngredient2
     p3.innerHTML = ingred.strIngredient3
@@ -76,26 +53,29 @@ function recipeIngredients(ingred) {
     p18.innerHTML = ingred.strIngredient18
     p19.innerHTML = ingred.strIngredient19
     p20.innerHTML = ingred.strIngredient20
-};
+};*/
 
 // Test run with Ingredient
 function igg(str) {
   for (let i = 1; i <= 20; i++){
+  	//if (listElement !== "") {
      const newItem = document.createElement("li");
      listElement.appendChild(newItem);
      newItem.innerHTML = str["strIngredient" + i];
 
   	//console.log(str["strIngredient" + i]);
+   //}
   }
 }
 
 
 
 function recipeVideo(vid) {
-   let video = document.getElementById("video").href = vid.strYoutube;
+   const video = document.getElementById("video").href = vid.strYoutube;
 };
 
-  function NewRecipe() {
+  function newRecipe() {
+  	listElement.innerHTML = ""
   axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
         .then(function (response) {
            const data = response.data.meals[0];
